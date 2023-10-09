9f1f7c7d345b19b5a36cae7c9bd08f6b3dc95e4e
Zac Detorakis <zacharias.detorakis@bjss.com>
2023-10-09 16:06:04 +0100

    this is a regular update

---
+++ day-61-starting-files-flask-secrets/main.py
@@ -0,0 +1,26 @@
+from flask import Flask, render_template
+
+'''
+Red underlines? Install the required packages first: 
+Open the Terminal in PyCharm (bottom left). 
+
+On Windows type:
+python -m pip install -r requirements.txt
+
+On MacOS type:
+pip3 install -r requirements.txt
+
+This will install the packages from requirements.txt for this project.
+'''
+
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def home():
+    return render_template('index.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
