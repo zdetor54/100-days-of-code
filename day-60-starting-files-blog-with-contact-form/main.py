ff662b4c4c7127f2e1e3539326721827fc21a6ea
Zac Detorakis <zacharias.detorakis@bjss.com>
2023-10-09 13:44:21 +0100

    this is a regular update

---
+++ day-60-starting-files-blog-with-contact-form/main.py
@@ -0,0 +1,41 @@
+from flask import Flask, render_template, request
+import requests
+
+# USE YOUR OWN npoint LINK! ADD AN IMAGE URL FOR YOUR POST. 👇
+posts = requests.get("https://api.npoint.io/c790b4d5cab58020d391").json()
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def get_all_posts():
+    return render_template("index.html", all_posts=posts)
+
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+
+@app.route("/contact", methods= ["POST", "GET"])
+def contact():
+    if request.method == 'POST':
+        return "<h1>Successfully sent your message</h1>"
+    else:
+        return render_template("contact.html")
+#
+# @app .route("/form-entry", methods= ["POST", "GET"])
+# def receive_data():
+#
+
+@app.route("/post/<int:index>")
+def show_post(index):
+    requested_post = None
+    for blog_post in posts:
+        if blog_post["id"] == index:
+            requested_post = blog_post
+    return render_template("post.html", post=requested_post)
+
+
+if __name__ == "__main__":
+    app.run(debug=True, port=5001)
---
+++ my-personal-site/main.py
@@ -0,0 +1,9 @@
+
+#generat random number from 1 to 100
+
+import random
+
+#generate random number from 1 to 100
+random_number = random.randint(1, 100)
+
+print(random_number)
